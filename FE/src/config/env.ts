d9aa3fd43d9f5af8ba882e50c95ff7f6a3f2024e
Nguyễn Minh Tú <minhtupm123@gmail.com>
2026-05-30 14:34:58 +0700

    chore: env.ts

--- FE/src/config/env.ts
+++ FE/src/config/env.ts
@@ -1,9 +1,14 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://3.25.150.62:5000';
+export const API_BASE_URL =
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    process.env.DEPLOY ||
+    '';
 
-export const AI_ASSISTANT_CACHE_KEY = 'latee:practice:ai-assistant-sidebar:messages';
+export const AI_ASSISTANT_CACHE_KEY =
+    'latee:practice:ai-assistant-sidebar:messages';
 
-export const PATIENT_CHAT_AND_VALIDATION_CACHE_KEY = 'latee:practice:patient-chat-and-validation';
+export const PATIENT_CHAT_AND_VALIDATION_CACHE_KEY =
+    'latee:practice:patient-chat-and-validation';
 
 export const env = {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://3.25.150.62:5000',
+    API_BASE_URL,
 };
